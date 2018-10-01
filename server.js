54df158e6a1d3596afa2e8c0a1cad56983eb2b36
abdnew <abdbourghli@gmail.com>
2018-10-01 23:03:08 +0300

    'compine'

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(morgan('combined'))
 
 app.use(express.static(__dirname + '/public'));
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
-	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
