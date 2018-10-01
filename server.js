a736514be7736ad3fc33073fa5e07c8f82d4dd03
abdnew <abdbourghli@gmail.com>
2018-10-01 13:31:34 +0300

    'test4'

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app= express();
-app.get('/',function(req,res){
-	res.send('hello world');
-});
+
+app.use(express.static(__dirname + '/public'));
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var   ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-app.listen(port,ip);
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
