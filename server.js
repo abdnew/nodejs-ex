7bf43ced12ff978b567b2c38e38a9752b3b60d98
abdnew <abdbourghli@gmail.com>
2018-10-02 23:26:18 +0300

    'test9'

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-//app.use(express.static(__dirname + '/public'));
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
@@ -77,7 +76,7 @@ var initDb = function(callback) {
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
- /* if (!db) {
+  if (!db) {
     initDb(function(err){});
   }
   if (db) {
@@ -88,12 +87,14 @@ app.get('/', function (req, res) {
       if (err) {
         console.log('Error running count. Message:\n'+err);
       }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+	  res.send('Hello World')
+   //   res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
-  }*/
-  res.send('Hello World')
+	  res.send('Hello World')
+   // res.render('index.html', { pageCountMessage : null});
+  }
+  
 });
 
 app.get('/pagecount', function (req, res) {
@@ -133,35 +134,7 @@ app.get('/api/developer', function(req, res){
 	res.json(developer);
 });
 
-const TwitchJS = require('twitch-js');
 
-    // Setup the client with your configuration; more details here:
-    // https://github.com/twitch-apis/twitch-js/blob/master/docs/Chat/Configuration.md
-    const options = {
-      channels: ["#abd_new"],
-      // Provide an identity
-       identity: {
-         username: "a_plus_bot",
-         password: "oauth:mv9wq9z2abi4v8mplmaz6ot2356i67"
-       },
-    };
-
-    const client = new TwitchJS.client(options);
-
-    // Add chat event listener that will respond to "!command" messages with:
-    // "Hello world!".
-    client.on('chat', (channel, userstate, message, self) => {
-      console
-        .log(`Message "${message}" received from ${userstate['display-name']}`);
-
-      // Do not repond if the message is from the connected identity.
-      if (self) return;
-
-      if (options.identity && message === '!command') {
-        // If an identity was provided, respond in channel with message.
-        client.say(channel, 'Hello world!');
-     }
-    });
 	
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
